43dc69dcf2113d68f92096ea7a661f34cee2dcf1
HaiNC <nguyenhai0527@gmail.com>
2023-10-13 12:19:19 +0700

    Config React Router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Banner from "./pages/banners/Banner";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Banner />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,36 @@
 import React from "react";
+import {
+  Container,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@mui/material";
+// import MenuIcon from "@mui/icons-material/Menu";
+
 import "./Home.css";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <Container maxWidth="xl">
+      <AppBar position="static">
+        <Toolbar className="tool-bar-cus">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h5" component="h1" sx={{ flexGrow: 2 }}>
+            FSB
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
 }
