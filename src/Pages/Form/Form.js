a52788cce64a706941324fca69db3adcf72b5b56
HaiNC <nguyenhai0527@gmail.com>
2023-10-14 09:48:22 +0700

    Add banner + assets folder

--- src/Pages/Form/Form.js
+++ src/Pages/Form/Form.js
@@ -8,6 +8,7 @@ import {
   DoneAllOutlined,
 } from "@mui/icons-material";
 
+import Banner from "../Banner/Banner";
 import DatePickers from "../Components/DatePicker/DatePickers";
 import TextFields from "../Components/TextField/TextFields";
 import Buttons from "../Components/Button/Buttons";
@@ -27,29 +28,32 @@ function Form({ icon, text }) {
   };
 
   return (
-    <Container fixed>
-      <Stack spacing={2}>
-        <h2>Survey Form</h2>
+    <>
+      <Banner />
+      <Container fixed>
+        <Stack spacing={2}>
+          <h2>Survey Form</h2>
 
-        <TextFields label="Full Name" />
-        <DatePickers label="Day of Birth" defaultValue="YY" />
-        <TextFields label="Email" icon={<AlternateEmailOutlined />} />
-        <TextFields label="Phone" icon={<PhoneIphoneOutlined />} />
-        <TextFields label="Position" icon={<ApartmentOutlined />} />
-        <br />
-        <Buttons
-          variant="contained"
-          color="success"
-          text="Submit"
-          endIcon={<DoneAllOutlined />}
-          onClick={() => {
-            alert("clicked");
-          }}
-        />
-        <Buttons href="/datalist" text="Data List" />
-        <Buttons href="/wheelspinner" text="Wheel Snipper" />
-      </Stack>
-    </Container>
+          <TextFields label="Full Name" />
+          <DatePickers label="Day of Birth" defaultValue="YY" />
+          <TextFields label="Email" icon={<AlternateEmailOutlined />} />
+          <TextFields label="Phone" icon={<PhoneIphoneOutlined />} />
+          <TextFields label="Position" icon={<ApartmentOutlined />} />
+          <br />
+          <Buttons
+            variant="contained"
+            color="success"
+            text="Submit"
+            endIcon={<DoneAllOutlined />}
+            onClick={() => {
+              alert("clicked");
+            }}
+          />
+          <Buttons href="/datalist" text="Data List" />
+          <Buttons href="/wheelspinner" text="Wheel Snipper" />
+        </Stack>
+      </Container>
+    </>
   );
 }
 
