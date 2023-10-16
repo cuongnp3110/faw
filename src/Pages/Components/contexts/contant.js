d592e064ec0020cab4009b3e12e56506120e0ad0
HaiNC <nguyenhai0527@gmail.com>
2023-10-16 21:48:02 +0700

    Config contant

--- src/Pages/Components/contexts/contant.js
+++ src/Pages/Components/contexts/contant.js
@@ -1,4 +1,6 @@
 export const apiURL =
-  process.env.NODE_ENV !== "production" ? "http://localhost:3001/api/v1" : "";
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3001/api/v1"
+    : "https://faw-2cd946993c1e.herokuapp.com/api/v1";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "fsb-localStorage";
